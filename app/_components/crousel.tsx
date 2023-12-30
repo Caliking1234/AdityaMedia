c2d127c40dc0ed1e3502cbfaf4ab39bc6b6fa33c
Piyush <piyyush1219@gmail.com>
2023-12-30 22:18:49 +0530

    landing page

---
+++ app/_components/crousel.tsx
@@ -0,0 +1,47 @@
+"use client";
+import * as React from "react";
+import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+export function CarouselPlugin() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  );
+
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className=" max-w-[800px] py-5"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <Image
+                  alt="img"
+                  src={`/images/img${index + 1}.jpg`}
+                  width={800}
+                  height={500}
+                />
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
---
+++ app/_components/navBar.tsx
@@ -0,0 +1,110 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Menu } from "lucide-react";
+
+const NavBar = () => {
+  const [menu, setmenu] = useState(false);
+  return (
+    <>
+      <nav className=" w-full h-[100px] bg-[#EDD0B3] p-4 flex-row items-center justify-between hidden md:flex">
+        <div className=" w-full flex flex-row items-center justify-start px-1">
+          <Image
+            src={"/images/adityaMediaLogoCopy.png"}
+            alt="Aaditya Media"
+            height={35}
+            width={35}
+          />
+          <h1 className=" text-xl text-[#EE6223] font-semibold px-2">
+            aaditya media.
+          </h1>
+        </div>
+        <div className=" w-full flex flex-row justify-end gap-3">
+          <Link
+            href="/about"
+            className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+          >
+            about us
+          </Link>
+          <Link
+            href="/service"
+            className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+          >
+            services
+          </Link>
+          <Link
+            href="/work"
+            className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+          >
+            our work
+          </Link>
+          <Link
+            href="/clients"
+            className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+          >
+            our clients
+          </Link>
+        </div>
+      </nav>
+      <nav className=" w-full h-[100px] bg-[#EDD0B3] p-4 flex-row items-center justify-between flex md:hidden">
+        <div className=" w-full flex flex-row items-center justify-between px-1">
+          <div className="flex flex-row items-center justify-center px-1">
+            <Image
+              src={"/images/adityaMediaLogoCopy.png"}
+              alt="Aaditya Media"
+              height={35}
+              width={35}
+            />
+            <h1 className=" text-xl text-[#EE6223] font-semibold px-2">
+              aaditya media.
+            </h1>
+          </div>
+          <div className=" text-[#EE6223]">
+            <Menu
+              onClick={() => {
+                setmenu(!menu);
+              }}
+            />
+          </div>
+        </div>
+        <div
+          className={
+            menu
+              ? " w-full bg-[#EDD0B3] absolute top-[100px] h-screen left-0 transition-all duration-300 ease-in z-50 py-5"
+              : " w-full bg-[#EDD0B3] absolute top-[100px] h-screen left-[-100vw] transition-all duration-300 ease-in z-50 py-5"
+          }
+        >
+          <div className=" w-full flex flex-col justify-center items-center gap-3">
+            <Link
+              href="/about"
+              className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+            >
+              about us
+            </Link>
+            <Link
+              href="/service"
+              className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+            >
+              services
+            </Link>
+            <Link
+              href="/work"
+              className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+            >
+              our work
+            </Link>
+            <Link
+              href="/clients"
+              className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+            >
+              our clients
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
