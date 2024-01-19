fb9790e62868d15b010940d31a3d29fe06a7a2ab
Piyush <piyyush1219@gmail.com>
2024-01-19 19:04:46 +0530

    ..

--- app/_components/navBar.tsx
+++ app/_components/navBar.tsx
@@ -83,6 +83,12 @@ const NavBar = () => {
         >
           <div className=" w-full flex flex-col justify-center items-center gap-3">
             <Link
+              href="/"
+              className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
+            >
+              home
+            </Link>
+            <Link
               href="/about"
               className="p-1 text-[#EE6223] hover:text-white hover:font-bold transition-all duration-100"
             >
