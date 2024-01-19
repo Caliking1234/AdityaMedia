e32e2391c323ef3c1c7f0a2034899cb8b3a379f1
Piyush <piyyush1219@gmail.com>
2024-01-19 20:39:05 +0530

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -57,29 +57,6 @@ export default function Home() {
               <b className="text-[#EE6223]">Programs</b>
             </p>
           </div>
-          <div className="p-5 w-full">
-            <h1 className=" text-xl text-[#EE6223] font-semibold p-1">
-              ...about Arjun Chopra
-            </h1>
-            <b className="text-[#EE6223]">17 Years</b> in the Event Industry IMG
-            , Ogilvy Activation , Percept D &apo;Mark ,{" "}
-            <b className="text-[#EE6223]">Bellset Entertainment</b>
-            Launched the <b className="text-[#EE6223]">V3 Razor </b>
-            <b className="text-[#EE6223]">Planned , Executed</b> The First
-            Formula 1 Event to be held in India the ING{" "}
-            <b className="text-[#EE6223]">– Renault F1 Roadshow</b> on Rajpath.
-            Responsible for{" "}
-            <b className="text-[#EE6223]">Founding India&apos;s first</b> Super
-            Car Club along{" "}
-            <b className="text-[#EE6223]">with Autocar Magazine</b>. Managed{" "}
-            <b className="text-[#EE6223]">the Panasonic Open Golf</b> , USD
-            300,000 dollar Prize Money event{" "}
-            <b className="text-[#EE6223]">held at the DGC</b> , shown live
-            globally in 2012 and 2013 to{" "}
-            <b className="text-[#EE6223]">208 Million Households</b>. Designed
-            and Built North India&apos;s Biggest Go Karting track as a{" "}
-            <b className="text-[#EE6223]">Turnkey project.</b>
-          </div>
         </div>
       </div>
     </main>
