2f21e170b3d43465c154e67f39ba43215c318d8b
Piyush <piyyush1219@gmail.com>
2024-01-21 10:35:05 +0530

    ..

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -9,153 +9,210 @@ const page = () => {
           Clients We served
         </h1>
       </div>
-      <div className=" p-1 w-full grid grid-cols-3">
-        <Image
-          src="/images/arvind-limited-logo.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/AT-logo-1024.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/BijliPay-Logo.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
+      <div className=" p-1 w-full grid grid-cols-3 items-center justify-center">
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto ">
+          <Image
+            src="/images/arvind-limited-logo.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/AT-logo-1024.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/BijliPay-Logo.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
       <div className=" p-2 w-full grid grid-cols-2">
-        <Image
-          src="/images/download (1).png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/download (2).png"
-          width={200}
-          height={50}
-          alt="src"
-        />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/download (1).png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/download (2).png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
       <div className=" p-3 w-full grid grid-cols-3">
-        <Image
-          src="/images/download (3).png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/download (4).png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image src="/images/download.png" width={200} height={50} alt="src" />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/download (3).png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/download (4).png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image src="/images/download.png" width={200} height={50} alt="src" />
+        </div>
       </div>
       <div className=" p-1 w-full grid grid-cols-2">
-        <Image src="/images/images.jpeg" width={200} height={50} alt="src" />
-        <Image
-          src="/images/kisspng-exercise-computer-icons-physical-fitness-dumbbell-5adba34e34ce15.2963410015243436302163.jpg"
-          width={200}
-          height={50}
-          alt="src"
-        />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image src="/images/images.jpeg" width={200} height={50} alt="src" />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/kisspng-exercise-computer-icons-physical-fitness-dumbbell-5adba34e34ce15.2963410015243436302163.jpg"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
       <div className=" p-2 w-full grid grid-cols-3">
-        <Image
-          src="/images/maxposure-media-group-logo-761C0D2655-seeklogo.com.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image src="/images/mercedese.jpeg" width={200} height={50} alt="src" />
-        <Image
-          src="/images/ndtv9182.logowik.com.webp"
-          width={200}
-          height={50}
-          alt="src"
-        />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/maxposure-media-group-logo-761C0D2655-seeklogo.com.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/mercedese.jpeg"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/ndtv9182.logowik.com.webp"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
       <div className=" p-3 w-full grid grid-cols-2">
-        <Image
-          src="/images/png-clipart-bhilai-steel-authority-of-india-company-tata-steel-sail-miscellaneous-blue.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/png-clipart-computer-keyboard-ipad-3-ipad-mini-logitech-ipad-pro-headphones-electronics-text.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/png-clipart-bhilai-steel-authority-of-india-company-tata-steel-sail-miscellaneous-blue.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/png-clipart-computer-keyboard-ipad-3-ipad-mini-logitech-ipad-pro-headphones-electronics-text.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
       <div className=" p-1 w-full grid grid-cols-3">
-        <Image
-          src="/images/png-clipart-dog-giant-panda-paw-dog-animals-paw.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/png-clipart-logo-superdry-clothing-shopping-brand-jacket-text-retail-thumbnail.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/png-clipart-Škoda-auto-car-Škoda-superb-Škoda-vision-x-skoda-text-logo.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/png-clipart-dog-giant-panda-paw-dog-animals-paw.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/png-clipart-logo-superdry-clothing-shopping-brand-jacket-text-retail-thumbnail.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/png-clipart-Škoda-auto-car-Škoda-superb-Škoda-vision-x-skoda-text-logo.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
       <div className=" p-2 w-full grid grid-cols-2">
-        <Image
-          src="/images/png-transparent-collective-hub-business-logo-graphic-designer-collective-company-text-people.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/png-transparent-panasonic-led-backlit-lcd-television-set-high-definition-television-panasonic-logo-television-blue-text.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/png-transparent-collective-hub-business-logo-graphic-designer-collective-company-text-people.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/png-transparent-panasonic-led-backlit-lcd-television-set-high-definition-television-panasonic-logo-television-blue-text.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
       <div className=" p-3 w-full grid-cols-3">
-        <Image
-          src="/images/pocpvkxyunutyw5t0idb.webp"
-          width={200}
-          height={50}
-          alt="src"
-        />
-        <Image
-          src="/images/Rolls_royce_motorcars_logo.svg.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/pocpvkxyunutyw5t0idb.webp"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/Rolls_royce_motorcars_logo.svg.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
       <div className=" p-1 w-full grid grid-cols-2">
-        <Image src="/images/toyota.jpeg" width={200} height={50} alt="src" />
-        <Image src="/images/unnamed.png" width={200} height={50} alt="src" />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image src="/images/toyota.jpeg" width={200} height={50} alt="src" />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image src="/images/unnamed.png" width={200} height={50} alt="src" />
+        </div>
       </div>
       <div className=" p-1 w-full grid grid-cols-2">
-        <Image src="/images/renault.jpeg" width={200} height={50} alt="src" />
-        <Image
-          src="/images/waveinfratech.png"
-          width={200}
-          height={50}
-          alt="src"
-        />
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image src="/images/renault.jpeg" width={200} height={50} alt="src" />
+        </div>
+        <div className=" w-[200px] shadow-md shadow-black p-3 flex items-center justify-center mx-auto">
+          <Image
+            src="/images/waveinfratech.png"
+            width={200}
+            height={50}
+            alt="src"
+          />
+        </div>
       </div>
     </div>
   );
