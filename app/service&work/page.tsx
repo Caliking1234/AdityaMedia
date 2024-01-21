f4629e43d523efb85968e95ed8d8db21d32cb721
Piyush <piyyush1219@gmail.com>
2024-01-21 20:41:10 +0530

    ..

--- app/service&work/page.tsx
+++ app/service&work/page.tsx
@@ -1,11 +1,79 @@
+"use client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Image from "next/image";
 import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
     <div className=" w-full h-full text-3xl flex justify-center items-center">
-      Under production...
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Image
+              alt="src"
+              src={"/images/intro.jpg"}
+              width={200}
+              height={200}
+              className=" w-[50px] h-[50px] rounded-full shadow-sm shadow-black"
+            />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Image
+              alt="src"
+              src={"/images/intro.jpg"}
+              width={200}
+              height={200}
+              className=" w-[50px] h-[50px] rounded-full shadow-sm shadow-black"
+            />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Image
+              alt="src"
+              src={"/images/intro.jpg"}
+              width={200}
+              height={200}
+              className=" w-[50px] h-[50px] rounded-full shadow-sm shadow-black"
+            />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Image
+              alt="src"
+              src={"/images/intro.jpg"}
+              width={200}
+              height={200}
+              className=" w-[50px] h-[50px] rounded-full shadow-sm shadow-black"
+            />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Image
+              alt="src"
+              src={"/images/intro.jpg"}
+              width={200}
+              height={200}
+              className=" w-[50px] h-[50px] rounded-full shadow-sm shadow-black"
+            />
+          </CarouselItem>
+          <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+            <Image
+              alt="src"
+              src={"/images/intro.jpg"}
+              width={200}
+              height={200}
+              className=" w-[50px] h-[50px] rounded-full shadow-sm shadow-black"
+            />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 };
 
-export default page;
+export default Page;
